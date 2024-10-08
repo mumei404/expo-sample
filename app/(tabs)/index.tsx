df8276827b0d64b1c0c7e3c49b3aac6ce0beb05b
mumei404 <lozld30@gmail.com>
2024-10-08 23:58:56 +0900

    image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import {Link} from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import PlaceholderImage from '../../assets/images/background-image.png';
+import ImageViewer from "@/components/ImageViewer";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-			<Link href="/about" style={styles.button}>
-				Go to About screen
-			</Link>
+			<View style={styles.imageContainer}>
+				<ImageViewer imgSource={PlaceholderImage} />
+			</View>
     </View>
   );
 }
@@ -17,14 +18,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#25292e",
 		alignItems: "center",
-		justifyContent: "center",
 	},
-	text: {
-		color: "#fff",
+	imageContainer: {
+		flex: 1,
 	},
-	button: {
-		fontSize: 20,
-		textDecorationLine: 'underline',
-		color: "#fff",
-	}
 })
