86e69588cc37d946cdd8de960d1e14bdf6e927b7
mumei404 <lozld30@gmail.com>
2024-10-11 00:10:43 +0900

    save photo for web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import * as MediaLibrary from 'expo-media-library'
+import domtoimage from 'dom-to-image'
 
 import PlaceholderImage from '../../assets/images/background-image.png';
 import ImageViewer from "@/components/ImageViewer";
@@ -53,18 +54,35 @@ export default function Index() {
 	}
 
 	const onSaveImageAsync = async () => {
-		try {
-			const localUri = await captureRef(imageRef, {
-				height: 440,
-				quality: 1,
-			})
+		if(Platform.OS !== "web") {
+			try {
+				const localUri = await captureRef(imageRef, {
+					height: 440,
+					quality: 1,
+				})
 
-			await MediaLibrary.saveToLibraryAsync(localUri)
-			if(localUri) {
-				alert('saved')
+				await MediaLibrary.saveToLibraryAsync(localUri)
+				if(localUri) {
+					alert('saved')
+				}
+			} catch(e) {
+				console.log(e)
 			}
-		} catch(e) {
-			console.log(e)
+		} else {
+			try {
+				const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+					quality: 0.95,
+					width: 320,
+					height: 440,
+				})
+
+				let link = document.createElement('a')
+				link.download = 'sticker-smash.jpeg'
+				link.href = dataUrl
+				link.click()
+			} catch(e) {
+				console.log(e)
+			}
 		}
 	}
 
